853eaf57aee1c1216e9c7b4861afabfa19977dc0
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-28 23:08:47 +0330

    for each page witch has  a diffrent header

---
+++ components/Base/BaseDashboardHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="flex flex-row-reverse bottom-gradient w-full pb-3 justify-between items-center"
+  >
+    <p>{{ pageName }}</p>
+    <div class="flex flex-row-reverse">
+      <img
+        class="ml-3 cursor-pointer"
+        src="../../assets/images/repeaticon.svg"
+      />
+      <div class="flex flex-row-reverse cursor-pointer head-button py-2 px-1.5">
+        <img class="ml-1" src="../../assets/images/usericon.svg" alt="" />
+        <span>مهدی عزیز خوش امدید</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pageName: {
+      type: String,
+      default: "داشبورد",
+    },
+  },
+};
+</script>
+
+<style></style>
