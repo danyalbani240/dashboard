d289d9976ea6d0f7add57cdec052c45c53e615a9
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-28 23:09:22 +0330

    status section of first page

---
+++ components/Base/BaseProgressBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex flex-row-reverse items-center">
+    <div class="w-96 h-5 relative progress-bar rounded-2xl mr-5">
+      <div
+        class="absolute flex justify-center text-sm progress w-6/12 h-full rounded-2xl text-primary"
+      >
+        ثبت نام اولیه
+      </div>
+      <div
+        class="w-9 h-9 flex items-center justify-center win absolute icon-container rounded-md"
+      >
+        <img src="../../assets/images/win.svg" alt="" />
+      </div>
+    </div>
+    <button class="auth-btn mr-12 rounded-md text-primary">احراز هویت</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.progress-bar {
+  border: 1px solid #15ab89;
+  z-index: 1;
+}
+.progress {
+  background-color: #15ab89;
+  box-shadow: 0px 0px 10px #15ab89;
+  right: 0;
+}
+.progress::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  width: 28px;
+  height: 32px;
+  top: -20px;
+  transform: translate(-50%, -50%);
+  background-image: url(../../assets/images/union.svg);
+}
+.win {
+  left: 0;
+  top: 0;
+  transform: translate(-50%, -25%);
+  z-index: 3;
+  background-image: linear-gradient(
+    to right,
+    rgba(255, 255, 255, 0.18) 0%,
+    rgba(196, 196, 196, 0.06) 100%
+  );
+}
+.auth-btn {
+  width: 140px;
+  height: 36px;
+  background-image: linear-gradient(90.93deg, #097369 0%, #15ab88 100%);
+}
+</style>
---
+++ components/TheSatusAndCredit.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="mt-14">
+    <div class="flex flex-row-reverse">
+      <div>
+        <base-part-header
+          :address="'usericon'"
+          :headName="'وضعیت حساب کاربری'"
+        />
+
+        <div
+          class="rounded-2xl flex flex-row-reverse relative bg-primary z-10 user-stat mt-2.5"
+        >
+          <div class="flex flex-col">
+            <div class="text-right mt-11 mr-5">
+              <p class="font-bold text-lg">.شما در سطح پایه هستید</p>
+              <p class="mt-2.5">
+                .با انجام دادن مراحل احراز هویت سطح خود را افزایش دهید
+              </p>
+            </div>
+            <div class="mt-12"><base-progress-bar /></div>
+          </div>
+          <div class="mt-6 mr-12">
+            <img src="../assets/images/stat-hero.svg" alt="" />
+          </div>
+        </div>
+      </div>
+      <!-- card -->
+      <div class="mr-14">
+        <base-part-header :address="'credit'" :headName="'کارت های بانکی'" />
+        <div class="credit-card relative mt-2.5">
+          <div class="flex justify-end">
+            <img
+              class="mr-5 mt-8"
+              src="../assets/images/card-icon.svg"
+              alt=""
+            />
+          </div>
+          <div class="flex items-center flex-col">
+            <p class="text-primary text-lg mb-3 font-bold">کارتی وجود ندارد</p>
+            <p
+              class="text-primary py-1.5 px-2 border-primary border-2 rounded-md"
+            >
+              + افزودن کارت
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BasePartHeader from "./Base/BasePartHeader.vue";
+export default {
+  components: { BasePartHeader },
+};
+</script>
+
+<style>
+p {
+  color: #ebebeb;
+}
+.user-stat {
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.35);
+  border: 1px solid rgba(21, 171, 137, 0.3);
+  width: 795px;
+  height: 210px;
+}
+.user-stat::after {
+  position: absolute;
+  content: "";
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 167px;
+  height: 159px;
+  background-color: #15ab89;
+  opacity: 0.8;
+  filter: blur(210px);
+  border-radius: 50%;
+}
+.credit-card {
+  background-image: url(../assets/images/card-bg.svg);
+  border-radius: 16px;
+  width: 330px;
+  height: 210px;
+  filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.74));
+}
+.credit-card::after {
+  border-radius: 50%;
+  width: 167px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  content: "";
+  height: 159px;
+  background-color: #15ab89;
+  opacity: 0.8;
+  filter: blur(210px);
+}
+</style>
