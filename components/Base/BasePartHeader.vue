ec1de112d344ce53d0dc2ecbac6c8be1c46311fb
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-28 15:14:18 +0330

    BAse Header Component created

---
+++ components/Base/BasePartHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-row-reverse items-center">
+    <div class="rounded-md icon-container p-1.5 ml-2.5">
+      <img :src="myImage" alt="" />
+    </div>
+    <p>{{ headName }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headName: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+  },
+  computed: {
+    //To making Image address dynamic
+    myImage() {
+      return require(`~/assets/images/partheader/${this.address}.svg`);
+    },
+  },
+};
+</script>
+
+<style></style>
