5457d9883cc1df902cb5a15e13ccbc2342e1c764
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-28 22:16:25 +0330

    added for popular coins

---
+++ components/Base/BaseListPopular.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex set justify-between">
+    <base-list-popular-item
+      v-for="(coin, index) in coins"
+      :key="coin.short"
+      :short="coin.short"
+      :allName="coin.allName"
+      :dPrice="coin.dPrice"
+      :buyPrice="coin.buyPrice"
+      :sellPrice="coin.sellPrice"
+      :trapezoidR="index % 2 == 0 ? false : true"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    coins: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.set:first-child {
+  position: relative;
+  right: -100px;
+}
+</style>
