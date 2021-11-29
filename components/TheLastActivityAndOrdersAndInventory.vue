be9c311b312a0dad9e89a41f05db2ec92c2c7664
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-29 18:55:52 +0330

    Orders tabled compoentnt added

--- components/TheLastActivityAndOrdersAndInventory.vue
+++ components/TheLastActivityAndOrdersAndInventory.vue
@@ -23,51 +23,7 @@
         <base-part-header :headName="'سفارشات'" :address="'shopingcart'" />
         <base-switch-badge linkOne="خرید" linkTwo="فروش" />
       </div>
-      <table
-        class="table relative orders rounded-2xl mt-2.5 font-xs"
-        style="color: #15ab89; direction: rtl"
-      >
-        <tr class="text-white justify-between">
-          <th class="rounded-tr-2xl">راز ارسالی</th>
-          <th>ارز دریافتی</th>
-          <th class="rounded-tl-2xl">تاریخ انجام</th>
-        </tr>
-        <tr>
-          <td>10 TRX</td>
-          <td>24.246.000 IRr</td>
-          <td>1400.7.12 10:53</td>
-        </tr>
-        <tr>
-          <td>10 TRX</td>
-          <td>24.246.000 IRr</td>
-          <td>1400.7.12 10:53</td>
-        </tr>
-        <tr>
-          <td>10 TRX</td>
-          <td>24.246.000 IRr</td>
-          <td>1400.7.12 10:53</td>
-        </tr>
-        <tr>
-          <td>10 TRX</td>
-          <td>24.246.000 IRr</td>
-          <td>1400.7.12 10:53</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td class="rounded-br-2xl"></td>
-          <td></td>
-          <td class="rounded-bl-2xl"></td>
-        </tr>
-      </table>
+      <the-orders-table />
     </div>
     <div class="mr-14">
       <div class="flex flex-row-reverse justify-between items-center">
@@ -117,7 +73,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .last-do {
   background-image: url("../assets/images/weiredelectral.svg?external");
   width: 390px;
---
+++ components/TheOrdersTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <table
+    class="table relative orders rounded-2xl mt-2.5 font-xs"
+    style="color: #15ab89; direction: rtl"
+  >
+    <tr class="text-white justify-between">
+      <th class="rounded-tr-2xl">راز ارسالی</th>
+      <th>ارز دریافتی</th>
+      <th class="rounded-tl-2xl">تاریخ انجام</th>
+    </tr>
+    <tr>
+      <td>10 TRX</td>
+      <td>24.246.000 IRr</td>
+      <td>1400.7.12 10:53</td>
+    </tr>
+    <tr>
+      <td>10 TRX</td>
+      <td>24.246.000 IRr</td>
+      <td>1400.7.12 10:53</td>
+    </tr>
+    <tr>
+      <td>10 TRX</td>
+      <td>24.246.000 IRr</td>
+      <td>1400.7.12 10:53</td>
+    </tr>
+    <tr>
+      <td>10 TRX</td>
+      <td>24.246.000 IRr</td>
+      <td>1400.7.12 10:53</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="rounded-br-2xl"></td>
+      <td></td>
+      <td class="rounded-bl-2xl"></td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
