65cca2b372f7de4ad5fa554441a29429b1f2b1ce
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-11-30 15:49:37 +0330

    Base Button Component Created

---
+++ components/Base/BaseButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    :class="{ 'bg-button-gradient': bGreen, 'border-bg': bTrans }"
+    class="px-9 py-2 rounded-md"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    bGreen: {
+      type: Boolean,
+    },
+    bTrans: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.bg-button-gradient {
+  background-image: linear-gradient(90.93deg, #097369 0%, #15ab88 100%);
+  color: #010e17;
+}
+.border-bg {
+  border: 1px solid #15ab89;
+}
+</style>
