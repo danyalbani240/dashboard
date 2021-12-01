6f6be3382c541e784c3748916c0536d4a5ad6700
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-12-01 23:24:03 +0330

    deposit rial page created

---
+++ pages/deposit-rial/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-primary w-full text-Neutral-Gray p-5">
+    <base-dashboard-header :pageName="'حسابداری'" />
+    <div class="mt-2">
+      <base-part-header :address="'insidereport'" headName="گزارش واریزی ها" />
+      <base-button>بازگشت</base-button>
+      <div class="mt-9 acounting-box-1 bg-primary py-6 px-12" dir="rtl">
+        <p>
+          مسئولیت مشکلات ناشی از عدم توجه به این موارد بر عهده‌ی مشتری خواهد
+          بود. جهت افزایش اعتبار کیف پول ریالی خود با استفاده از کارت‌های بانکی
+          عضو شبکه شتاب و از طریق درگاه پرداخت اینترنتی اقدام نمایید.
+        </p>
+        <p>در هنگام پرداخت به نکات زیر دقت نمایید:</p>
+        <p class="list-paragraph relative">
+          حتماً به آدرس صفحه‌ی درگاه بانکی دقت نموده و تنها پس از اطمینان از
+          حضور در سایت‌های سامانه‌ی شاپرک مشخصات کارت بانکی خود را وارد نمایید.
+        </p>
+        <p class="list-paragraph relative">
+          در صفحه درگاه دقت کنید که حتما مبلغ نمایش داده شده درست باشد.
+        </p>
+        <p class="list-paragraph relative">
+          در تعیین مبلغ واریز به این موضوع دقت نمایید که حداقل مبلغ معامله در
+          بازار نوین تکس سیصد هزار تومان است.
+        </p>
+        <p class="text-Neutral-Orange">
+          جهت واریز وجه، حتما باید از کارت‌های بانکی به نام خودتان که در
+          پروفایل‌تان ثبت و تایید شده است، استفاده نمایید.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
