c18cd1d7eb64cedb083012826d912e8abbea1ac6
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-12-04 17:46:32 +0330

    BAse INput component created

---
+++ components/Base/BaseInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <div dir="rtl">
+    <label class="mb-1">
+      {{ label }}
+    </label>
+    <input
+      type="text"
+      :placeholder="place"
+      v-bind:value="model"
+      v-on:input="model = $event"
+      :class="{ 'w-270': w270, 'w-366': w366, 'w-174': w174 }"
+      class="bg-primary"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    model: {
+      type: String,
+    },
+    place: {
+      type: String,
+    },
+    w270: {
+      type: Boolean,
+      default: false,
+    },
+    w174: {
+      type: Boolean,
+      default: false,
+    },
+    w366: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+    },
+  },
+};
+</script>
+<style>
+.w-270 {
+  width: 270px;
+}
+.w-366 {
+  width: 366px;
+}
+input {
+  border: 1px solid #15ab89;
+  border-radius: 5px;
+  height: 40px;
+}
+.w-174 {
+  width: 174px;
+}
+</style>
