2c0be026ddcd930713e25a6a1dd0440ee1edb56c
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-12-04 17:46:12 +0330

    prifle page created

---
+++ pages/profile/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-primary w-full text-Neutral-Gray p-5">
+    <base-dashboard-header :pageName="'پروفایل'" />
+    <div class="mt-2">
+      <base-part-header :address="'insidereport'" headName="حساب کاربری" />
+
+      <the-profile-form />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheProfileForm from "~/components/TheProfileForm.vue";
+export default {
+  components: { TheProfileForm },
+};
+</script>
+
+<style></style>
