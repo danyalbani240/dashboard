e9e876c97b6df0b7ee23c92977bea1f3a138b952
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-12-06 20:18:40 +0330

    verify page

---
+++ pages/verify/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="bg-primary w-full text-Neutral-Gray p-5">
+    <base-dashboard-header :pageName="'احراز هویت'" />
+
+    <div class="stages flex mt-8 justify-center">
+      <div class="stage mx-2.5">
+        <div class="stage-input"></div>
+        <p class="text-center mt-2">+</p>
+      </div>
+      <div class="stage mx-2.5">
+        <div class="stage-input"></div>
+        <p class="text-center mt-2">+</p>
+      </div>
+      <div class="stage mx-2.5">
+        <div class="stage-input"></div>
+        <p class="text-center mt-2">+</p>
+      </div>
+      <div class="stage mx-2.5">
+        <div class="stage-input"></div>
+        <p class="text-center mt-2">+</p>
+      </div>
+      <div class="stage mx-2.5">
+        <div class="stage-input"></div>
+        <p class="text-center mt-2">+</p>
+      </div>
+    </div>
+    <!-- <the-verify-stage-one /> -->
+    <!-- <the-verify-stage-two /> -->
+    <the-verify-email-stage />
+  </div>
+</template>
+
+<script>
+import TheVerifyEmailStage from "~/components/TheVerifyEmailStage.vue";
+import TheVerifyStageOne from "~/components/TheVerifyStageOne.vue";
+import TheVerifyStageTwo from "~/components/TheVerifyStageTwo.vue";
+export default {
+  components: { TheVerifyStageOne, TheVerifyStageTwo, TheVerifyEmailStage },
+};
+</script>
+
+<style>
+.stage-input {
+  width: 191px;
+  height: 10px;
+  border: 2px solid #15ab89;
+  filter: drop-shadow(0px 0px 6px #15ab89);
+  border-radius: 15.5px;
+  opacity: 0.65;
+  position: relative;
+}
+.stage-input::before {
+  content: "";
+  position: absolute;
+
+  width: 36px;
+  height: 36px;
+  background-repeat: no-repeat;
+  background: linear-gradient(
+    91.44deg,
+    rgba(255, 255, 255, 0.18) 0%,
+    rgba(196, 196, 196, 0.06) 100%
+  );
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 5px;
+}
+.stage-input::after {
+  background-image: url(../../assets/images/bulk-security-card.svg);
+  content: "";
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  background-repeat: no-repeat;
+  left: 50%;
+  top: -50%;
+  transform: translate(-35%, -25%);
+}
+</style>
