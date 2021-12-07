b302b9d1598f6e264bb8b871e764066cb7a370b2
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-12-07 14:22:49 +0330

    base crypto wallet item added to page

--- pages/crypto-wallet/index.vue
+++ pages/crypto-wallet/index.vue
@@ -1,13 +1,18 @@
 <template>
-  
+  <div class="bg-primary w-full text-Neutral-Gray p-5">
+    <base-dashboard-header :pageName="'کیف پول'" />
+    <div class="mt-2">
+      <base-part-header :address="'insidereport'" headName="کیف پول رمز ارز" />
+      <base-crypto-wallet-item />
+    </div>
+  </div>
 </template>
 
 <script>
+import BaseCryptoWalletItem from "~/components/Base/BaseCryptoWalletItem.vue";
 export default {
-
-}
+  components: { BaseCryptoWalletItem },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
