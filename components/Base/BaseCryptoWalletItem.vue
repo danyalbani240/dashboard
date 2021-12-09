24c9b7953da5ec78a2629f24fb48e73ee062cb6c
Danidev.ir <59335477+danyalbani240@users.noreply.github.com>
2021-12-09 11:32:59 +0330

    BAse Crypto BAse Item Created

--- components/Base/BaseCryptoWalletItem.vue
+++ components/Base/BaseCryptoWalletItem.vue
@@ -1,22 +1,56 @@
 <template>
   <div dir="rtl" class="base-item-container relative">
-    <div style="padding:0 121px;" class="base-item">
-      <div class="h-10 flex items-center">
-        <div
-          style="background-color: #010e17;border: 1px solid #15ab89;box-shadow: -4px 4px 10px rgba(0, 0, 0, 0.25);border-radius: 5px;"
-          class="h-10 w-10 flex justify-center items-center relative z-10"
-        >
+    <div class="base-item relative z-10 justify-between flex pt-5 items-center">
+      <div class="h-10 flex items-center invent-primary justify-between">
+        <div class="h-10 w-10 flex justify-center items-center relative z-10">
           <img src="../../assets/images/walletcheck.svg" alt="" />
         </div>
-        <div class="real-invent -mr-1 relative z-0 justify-between text-sm flex items-center px-4">
-            <span class="inline-block">موجودی اصلی</span>
-            <span>96.222</span>
+        <div
+          class="-mr-1 relative z-0 justify-between text-sm flex items-center px-4"
+        >
+          <span class="inline-block">موجودی اصلی</span>
+          <span>96.222</span>
         </div>
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div class="h-10 flex items-center invent-primary">
+        <div class="h-10 w-10 flex justify-center items-center relative z-10">
+          <img src="../../assets/images/empty-walletchange.svg" alt="" />
+        </div>
+        <div
+          class="-mr-1 relative z-0 justify-between text-sm flex items-center px-4"
+        >
+          <span class="inline-block">موجودی اصلی</span>
+          <span>96.222</span>
+        </div>
+      </div>
+      <div
+        class="flex items-center deposit h-8 w-24 justify-between text-center"
+      >
+        <span class="flex-1">واریز</span>
+        <span class="relative"
+          ><img
+            class="relative"
+            src="../../assets/images/downbutton.svg"
+            alt=""
+        /></span>
+      </div>
+      <div
+        class="flex items-center harvest h-8 w-24 justify-between text-center"
+      >
+        <span class="flex-1">برداشت</span>
+        <span class="relative"
+          ><img class="relative" src="../../assets/images/upbutton.svg" alt=""
+        /></span>
+      </div>
+      <div class="flex items-center trade h-8 w-24 justify-between text-center">
+        <span class="flex-1">ترید</span>
+        <span class="relative"
+          ><img
+            class="relative"
+            src="../../assets/images/trade-button.svg"
+            alt=""
+        /></span>
+      </div>
     </div>
   </div>
 </template>
@@ -35,11 +69,19 @@ export default {};
   width: 1180px;
   height: 98px;
   border: 2px solid #15ab89;
+  padding-right: 121px;
+  padding-left: 121px;
   /* box-shadow: inset 0px 0px 10px #15ab89; */
   /* filter: drop-shadow(0px 0px 10px #15ab89); */
   border-radius: 10px;
 }
-.real-invent {
+.invent-primary > :first-child {
+  background-color: #010e17;
+  border: 1px solid #15ab89;
+  box-shadow: -4px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+}
+.invent-primary > :nth-child(2) {
   background-image: linear-gradient(
     91.44deg,
     rgba(255, 255, 255, 0.18) 0%,
@@ -49,5 +91,29 @@ export default {};
   border-radius: 3px;
   width: 189px;
   height: 32px;
+}
+.deposit {
+  background-image: linear-gradient(
+    81.72deg,
+    rgba(31, 42, 50, 0) 6.35%,
+    #15ab88 93.65%
+  );
+  border-radius: 5px;
+}
+.harvest {
+  background-image: linear-gradient(
+    270deg,
+    #b81414 0%,
+    rgba(31, 42, 50, 0) 100%
+  );
+  border-radius: 5px;
+}
+.trade {
+  background-image: linear-gradient(
+    87.55deg,
+    rgba(36, 47, 55, 0) 1.6%,
+    #144bb8 98.6%
+  );
+  border-radius: 5px;
 }
 </style>
