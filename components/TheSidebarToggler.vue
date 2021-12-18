ccccc823af4b0bdd0708d9a9b8ea2f0278afc986
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-19 00:53:50 +0330

    adding vue x to sidebar toggler component

--- components/TheSidebarToggler.vue
+++ components/TheSidebarToggler.vue
@@ -1,8 +1,8 @@
 <template>
-  <div @click="toggleSidebar" class="toggle  inline-flex items-center justify-center  cursor-pointer">
+  <div @click="$store.commit('sidebar/toggleSidebar')" class="toggle  inline-flex items-center justify-center z-40 sm:static absolute sm:ml-2  cursor-pointer">
         <img src="../assets/images/toggleicon.svg" alt="">
     </div>
-</template>
+</template> 
 
 <script>
 export default {
@@ -21,8 +21,9 @@ height: 32px;
 background: #15AB89;
 box-shadow: 0px 0px 10px #15AB89;
 border-radius: 5px; 
-
-z-index: 20;
+top: -6px;
+right: 15px;
+z-index: 41;
 /* transform: translateX(-50%); */
   transition: all 0.4s ease-in;
 
