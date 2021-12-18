7a093ba4265988c1728586c88c3d5665a22dc0c5
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-18 15:47:18 +0330

    creating event bus from here

--- components/TheSidebarToggler.vue
+++ components/TheSidebarToggler.vue
@@ -6,10 +6,30 @@
 
 <script>
 export default {
-
+    methods:{
+        toggleSidebar(){
+            this.$nuxt.$emit('toggleSidebar')
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.toggle{
+ width: 32px;
+height: 32px; 
+background: #15AB89;
+box-shadow: 0px 0px 10px #15AB89;
+border-radius: 5px; 
 
+z-index: 20;
+/* transform: translateX(-50%); */
+  transition: all 0.4s ease-in;
+
+}
+.toggle.closed{
+  transform: translateX(-100%);
+  transition: all 0.4s ease-in;
+
+}
 </style>
