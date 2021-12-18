ca4eb90beba4235e8ab9bf96ee2e053f55e62391
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-18 15:22:12 +0330

    Sidebar Toggle Button Component Created

---
+++ components/TheSidebarToggler.vue
@@ -0,0 +1,15 @@
+<template>
+  <div @click="toggleSidebar" class="toggle  inline-flex items-center justify-center  cursor-pointer">
+        <img src="../assets/images/toggleicon.svg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
