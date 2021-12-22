091b810b39ee1f8548c0dce39f95e46d1f585e45
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-22 10:00:06 +0330

    Base List item component created

---
+++ components/Base/BaseListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div dir="rtl" class="flex items-baseline">
+      <img src="../../assets/images/lastpicon.svg" alt="">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
