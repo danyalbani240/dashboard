1761ef1bb962ac55df1cd40f7ae0470099e9a069
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-20 22:23:46 +0330

    BAse Sub Nav is Done✅

---
+++ components/Base/BaseSubNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <nuxt-link :to="linkAdress"
+    class="sub-nav flex flex-row-reverse pr-10 h-11 items-center cursor-pointer "
+  >
+    {{ name }}
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    linkAdress:{
+      type:String,
+      required:true
+    }
+  },
+};
+</script>
+
+<style>
+.sub-nav:hover {
+  border-right: 2px solid #15ab89;
+}
+</style>
