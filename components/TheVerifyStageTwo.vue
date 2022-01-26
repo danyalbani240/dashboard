511cb6fb5a45c79253dcb6453d66065d49dad89c
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-26 13:34:49 +0330

    bg added

--- components/TheVerifyStageTwo.vue
+++ components/TheVerifyStageTwo.vue
@@ -1,16 +1,38 @@
 <template>
-  <div class="verify-stage mx-auto mt-52 px-14 flex" dir="rtl">
-    <div style="width: 389px" class="flex-1">
-      <p class="font-bold text-lg text-center mt-9">تایید شماره</p>
-      <p class="text-center mt-2.5">.شماره موبایل خود را تایید و احراز کنید</p>
-      <base-input label="شماره تلفن همراه" :code="true" />
-      <base-input label="کدتایید" />
-      <base-button @click="$emit('getPass')" class="mt-4" :bGreen="true" :full="true"
-        >مرحله بعدی</base-button
-      >
+  <div>
+    <div class="verify-stage mx-auto mt-52 px-14 flex" dir="rtl">
+      <div style="width: 389px" class="flex-1">
+        <p class="font-bold text-lg text-center mt-9">تایید شماره</p>
+        <p class="text-center mt-2.5" dir="rtl">
+          شماره موبایل خود را تایید و احراز کنید.
+        </p>
+        <base-input
+          :placeGrey="true"
+          :place="'0925738158'"
+          label="شماره تلفن همراه"
+          :code="true"
+          class="mt-5"
+        />
+        <base-input
+          :placeGrey="true"
+          class="mt-10"
+          :place="'0925738158'"
+          label="کدتایید:"
+        />
+        <base-button
+          @click="$emit('getPass')"
+          class="mt-6"
+          :bGreen="true"
+          :full="true"
+          ><span class="text-white">مرحله بعدی</span></base-button
+        >
+      </div>
+      <div class="md:flex hidden items-center justify-center mr-28">
+        <img src="../assets/images/call.svg" alt="" />
+      </div>
     </div>
-    <div class="md:flex hidden items-center justify-center  mr-28">
-      <img src="../assets/images/call.svg" alt="" />
+    <div class="mt-20 md:block hidden">
+      <img class="mx-auto" src="../assets/images/stagetwo-bg.svg" alt="" />
     </div>
   </div>
 </template>
