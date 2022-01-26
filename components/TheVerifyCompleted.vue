1c26760310436399dd0439dd0b984e1723457f62
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-26 14:23:10 +0330

    lottie added

--- components/TheVerifyCompleted.vue
+++ components/TheVerifyCompleted.vue
@@ -2,19 +2,37 @@
   <div>
     <div class="flex flex-col justify-center items-center mt-32">
       <p class="text-lg">احراز هویت شما با موفقیت انجام شد</p>
-      <img class="mt-16" src="../assets/images/completed.svg" alt="" />
-      <p class="mt-16 cursor-pointer">
-        https://lottiefiles.com/70898-completed
-      </p>
+      <lottie
+        :options="lottieOptions"
+        :width="300"
+        v-on:animCreated="handleAnimation"
+      />
     </div>
-    <base-button @click="$router.push('/')" class="w-36 mt-11" :bGreen="true">پایان</base-button>
+
+    <base-button @click="$router.push('/')" class="w-36 mt-11" :bGreen="true"
+      >پایان</base-button
+    >
   </div>
 </template>
 
 <script>
 import BaseButton from "./Base/BaseButton.vue";
+import lottie from "vue-lottie/src/lottie.vue";
+import * as animationData from "~/assets/animations/animation.json";
+
 export default {
-  components: { BaseButton },
+  components: { BaseButton, lottie },
+  data() {
+    return {
+      anim: null, // for saving the reference to the animation
+      lottieOptions: { animationData: animationData.default },
+    };
+  },
+  methods: {
+    handleAnimation: function (anim) {
+      this.anim = anim;
+    },
+  },
 };
 </script>
 
