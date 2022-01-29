29c3349d16a84117c799d6809b927160eb55a5ff
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-29 21:29:58 +0330

    making the status in the same line, re responsiving it again, making the card bigger

--- components/TheSatusAndCredit.vue
+++ components/TheSatusAndCredit.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-14">
     <div class="flex flex-wrap flex-row-reverse justify-around">
-      <div class="flex-1 flex-col flex">
+      <div class="flex-1 pt-2 flex-col flex">
         <div dir="rtl">
           <base-part-header
             dir="ltr"
@@ -11,21 +11,23 @@
           />
 
           <div
-            class="rounded-2xl flex flex-row px-6 sm:px-2 bg-primary w-10/12 z-10 user-stat mt-2.5"
+            class="rounded-2xl flex flex-row px-6 sm:px-2 bg-primary mx-auto w-11/12 xl:w-10/12 z-10 lg:mx-0 user-stat mt-2.5"
           >
-            <div class="flex flex-col">
+            <div class="flex flex-col w-9/12 xl:flex-1">
               <div class="text-right mt-6 sm:mt-11 sm:mr-5">
                 <p class="font-bold sm:text-lg text-sm">
                   .شما در سطح پایه هستید
                 </p>
                 <p class="mt-3 text-xs sm:text-base sm:w-auto w-48">
-                  .با انجام دادن مراحل احراز هویت سطح خود را افزایش دهید
+                  با انجام دادن مراحل احراز هویت سطح خود را افزایش دهید.
                 </p>
               </div>
-              <div class="sm:mt-12 mt-2.5"><base-progress-bar /></div>
+              <div class="sm:mt-12 flex flex-1 md:flex-none mt-2.5">
+                <base-progress-bar />
+              </div>
             </div>
             <div
-              class="sm:mt-6 items-center sm:mr-12 flex-1 flex justify-center"
+              class="sm:mt-6 items-center sm:mx-12 flex-1 lg:flex-none flex justify-end"
             >
               <img src="../assets/images/stat-hero.svg" alt="" />
             </div>
@@ -33,9 +35,9 @@
         </div>
       </div>
       <!-- card -->
-      <div class="sm:mr-14 py-2 flex flex-col justify-center">
+      <div class="sm:mr-14 hidden w-3/12 py-2 lg:flex flex-col justify-center">
         <base-part-header :address="'credit'" :headName="'کارت های بانکی'" />
-        <div class="credit-card relative mt-2.5">
+        <div class="credit-card bg-cover w-full relative mt-2.5">
           <div class="flex justify-end">
             <img
               class="mr-5 mt-8"
@@ -72,7 +74,7 @@ p {
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.35);
   border: 1px solid rgba(21, 171, 137, 0.3);
 
-  height: 210px;
+  height: 212px;
 }
 @media only screen and (max-width: 600px) {
   .user-stat {
@@ -99,7 +101,6 @@ p {
 .credit-card {
   background-image: url(../assets/images/card-bg.svg);
   border-radius: 16px;
-  width: 330px;
   height: 210px;
   filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.74));
 }
