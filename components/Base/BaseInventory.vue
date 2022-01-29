1ca93e72d10d342b6f20586906f400e0826d2223
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-29 21:31:01 +0330

    make a change in the text, more padding mx

--- components/Base/BaseInventory.vue
+++ components/Base/BaseInventory.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="px-3 mt-2.5 ineventory rounded-2xl current">
-    <div class="flex relative p-2 sync justify-between">
-      <p>رمز ارز</p>
+  <div class="px-5 mt-2.5 ineventory rounded-2xl current">
+    <div class="flex relative p-2 syncer justify-between">
+      <p v-if="active == 1">رمز ارز</p>
+      <p v-else>ریالی</p>
       <p>موجودی</p>
     </div>
     <div class="flex flex-col mt-2.5">
@@ -48,5 +49,19 @@ export default {
   background-color: #15ab89;
   opacity: 0.5;
   filter: blur(210px);
+}
+.syncer::after {
+  position: absolute;
+  width: 100%;
+  content: "";
+  height: 2px;
+  background-image: linear-gradient(
+    270deg,
+    #010e17 0%,
+    #15ab89 51.04%,
+    #010e17 100%
+  );
+  left: 0;
+  bottom: 0px;
 }
 </style>
