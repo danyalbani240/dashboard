f524a4db41d028159bc68f8c095dcfd72fc561a7
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-02 14:18:42 +0330

    making the responsive of descriibe section ok

--- pages/deposit-rial/index.vue
+++ pages/deposit-rial/index.vue
@@ -2,31 +2,37 @@
   <div class="bg-primary w-full text-Neutral-Gray p-5">
     <base-dashboard-header :pageName="'حسابداری'" />
     <div class="mt-2">
-      <base-part-header :address="'insidereport'" headName="گزارش واریزی ها" />
-      <base-button @click="goback">بازگشت</base-button>
+      <base-part-header :address="'insidereport'" headName="واریز ریالی" />
+      <base-button :prev="true" @click="goback">بازگشت</base-button>
       <div class="relative">
         <div class="absolute bg-blur"></div>
-        <div class="mt-9 acounting-box-1 bg-primary py-6 px-12" dir="rtl">
-          <p>
+        <div class="mt-9 acounting-box-1 bg-primary py-6" dir="rtl">
+          <p class="px-12">
             مسئولیت مشکلات ناشی از عدم توجه به این موارد بر عهده‌ی مشتری خواهد
             بود. جهت افزایش اعتبار کیف پول ریالی خود با استفاده از کارت‌های
             بانکی عضو شبکه شتاب و از طریق درگاه پرداخت اینترنتی اقدام نمایید.
           </p>
-          <p>در هنگام پرداخت به نکات زیر دقت نمایید:</p>
-          <p class="list-paragraph relative mr-10">
-            حتماً به آدرس صفحه‌ی درگاه بانکی دقت نموده و تنها پس از اطمینان از
-            حضور در سایت‌های سامانه‌ی شاپرک مشخصات کارت بانکی خود را وارد
-            نمایید.حداکثر مبلغ برداشت ریالی در هر شبانه روز دویصد میلیون تومان
-            می باشد
-          </p>
-          <p class="list-paragraph relative mr-10">
-            در صفحه درگاه دقت کنید که حتما مبلغ نمایش داده شده درست باشد.
-          </p>
-          <p class="list-paragraph relative mr-10">
-            در تعیین مبلغ واریز به این موضوع دقت نمایید که حداقل مبلغ معامله در
-            بازار نوین تکس سیصد هزار تومان است.
-          </p>
-          <p class="text-Neutral-Orange">
+          <p class="px-12">در هنگام پرداخت به نکات زیر دقت نمایید:</p>
+          <BaseListItem>
+            <p class="relative pl-6">
+              حتماً به آدرس صفحه‌ی درگاه بانکی دقت نموده و تنها پس از اطمینان از
+              حضور در سایت‌های سامانه‌ی شاپرک مشخصات کارت بانکی خود را وارد
+              نمایید.حداکثر مبلغ برداشت ریالی در هر شبانه روز دویست میلیون تومان
+              می باشد
+            </p>
+          </BaseListItem>
+          <BaseListItem>
+            <p class="relative pl-6">
+              در صفحه درگاه دقت کنید که حتما مبلغ نمایش داده شده درست باشد.
+            </p>
+          </BaseListItem>
+          <BaseListItem>
+            <p class="relative pl-6">
+              در تعیین مبلغ واریز به این موضوع دقت نمایید که حداقل مبلغ معامله
+              در بازار نوین تکس سیصد هزار تومان است.
+            </p>
+          </BaseListItem>
+          <p class="text-Neutral-Orange px-12">
             جهت واریز وجه، حتما باید از کارت‌های بانکی به نام خودتان که در
             پروفایل‌تان ثبت و تایید شده است، استفاده نمایید.
           </p>
