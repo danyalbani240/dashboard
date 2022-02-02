edd3e04404d4174b283437dce9d91eb4356ab8f9
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-02 14:10:23 +0330

    Base Copy Message component created

---
+++ components/Base/BaseCopyMessage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="copy-message fixed bottom-20 bg-accent rounded p-4">
+    TextCopied
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.copy-message {
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
